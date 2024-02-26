f5930113507ae4a1e0576e552d5e0e05d65da996
Erikson <eriksonribeiro84@gmail.com>
2024-02-25 23:17:21 -0300

    starting code for Tasks screen

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { GridComponent, ColumnsDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
 
+import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
+import { Header } from '../components';
+
 const Tasks = () => {
   return (
-    <div className='bg-color-gray-500'>Tasks</div>
+    <div className='m-2 md:md-10 p-2 md:p-10 bg-white rounded-3x1'>
+      <Header category="Page" title="Tasks"/>
+      <GridComponent>
+        <ColumnsDirective>
+        {ordersGrid.map((item, index) => (
+          <ColumnsDirective key={index} {...item}/>
+        ))}
+        </ColumnsDirective>
+      </GridComponent>
+    </div>
   )
 }
 
