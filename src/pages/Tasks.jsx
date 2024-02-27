20a1710751e96a79970c0164e9080e432c5a24d9
Erikson <eriksonribeiro84@gmail.com>
2024-02-26 23:38:36 -0300

    orders/tasks page

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GridComponent, ColumnsDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
 
 import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
 import { Header } from '../components';
@@ -8,12 +8,16 @@ const Tasks = () => {
   return (
     <div className='m-2 md:md-10 p-2 md:p-10 bg-white rounded-3x1'>
       <Header category="Page" title="Tasks"/>
-      <GridComponent>
+      <GridComponent
+      id='gridcomp'
+      dataSource={ordersData}
+      allowPaging
+      allowSorting
+      >
         <ColumnsDirective>
-        {ordersGrid.map((item, index) => (
-          <ColumnsDirective key={index} {...item}/>
-        ))}
+        {ordersGrid.map((item, index) => (<ColumnDirective key={index} {...item} />))}
         </ColumnsDirective>
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport]}/>
       </GridComponent>
     </div>
   )
